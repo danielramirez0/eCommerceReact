eb1d8b29f53dd00b0d4e27e18f7c594bf5557972
Daniel Ramirez <zoerunner@gmail.com>
2021-11-24 13:32:56 -0800

    Removed react default items Add bootstrap 5.1.3 Move images and logos to img Add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React, {useEffect, useState } from "react";
+import {Routes, Route} from "react-router-dom";
+// import "bootswatch/dist/SELECT STYLE HERE"
 import './App.css';
+import Login from "./Components/Login"
+import Register from "./Components/Register"
+import Seller from "./Components/Seller"
+import Customer from "./Components/Customer"
 
 function App() {
+    const [backendURL] = useState("https://localhost:44394/api/");
+    const [authenticated, setAuthenticated] = useState(localStorage.getItem("ElectronifyJWT"))
+
+    function toggleAuth() {
+        setAuthenticated(!authenticated)
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className="container">
+              <Routes>
+                  <Route path="/" exact element={<Login />}/>
+                  <Route path="/register" exact element={<Register />}/>
+                  <Route path="/seller" exact element={<Seller />}/>
+                  <Route path="/customer" exact element={<Customer />}/>
+              </Routes>
+          </div>
       </header>
     </div>
   );
