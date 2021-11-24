e074b5f2f9937cd7446a19fa6179df5157fa2e73
Ben Bissett <benjaminbissett@gmail.com>
2021-11-24 10:39:57 -0700

    Updated product card.

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,14 +1,19 @@
 const ProductCard = (props) => {
     
     return (
-        <div style={{ maxWidth: "15rem" }}>
-            <div className="card-header">
-                <h3>{props.Name}</h3>
-                <p>{props.Description}</p>
-                <h4>{props.price}</h4>
-            </div>
-            <button className="btn btn-primary" onClick={() => addItemToCart()}>Add to Cart</button>
-        </div>
+        <ReactFragment>
+            {props.products.map((product) => {
+                <div style={{ maxWidth: "15rem" }}>
+                    <div className="card-header">
+                        <img src="#" alt="#" />
+                        <h3>{product.Name}</h3>
+                        <p>{product.Description}</p>
+                        <h4>{product.Price}</h4>
+                    </div>
+                    <button className="btn btn-primary" onClick={() => addItemToCart()}>Add to Cart</button>
+                </div>      
+            })}
+        </ReactFragment>
     )
 }
 
