516b30f6b4e91234c078c71b7245c748d9c0fb5d
Daniel Ramirez <zoerunner@gmail.com>
2021-11-29 09:48:22 -0800

    Adds functions for multiple search methods

--- src/Components/Customer/Customer.jsx
+++ src/Components/Customer/Customer.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import FlexNav from "../FlexNav/FlexNav";
 import logo from "../../img/LogoWithName.svg";
@@ -6,18 +6,60 @@ import ProductDisplay from "../ProductDisplay/ProductDisplay";
 import { BaseURLContext } from "../../baseURL-context";
 import { defaultGetRequest } from "../../static/functions";
 import { Card } from "react-bootstrap";
+import useForm from "../useForm/useForm";
 
 const Customer = () => {
+    const { values, handleChange, handleSubmit } = useForm(searchByName);
+    const [categories, setCategories] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [shoppingCart, setShoppingCart] = useState([]);
+    const [total, setTotal] = useState(0);
     const { baseURL } = useContext(BaseURLContext);
-    let products = [];
-    let shoppingCart = [];
-    let sum = 0;
-    let links = [];
+    const [filter, setFilter] = useState("all");
+
+    useEffect(() => {
+        setDisplayProducts(filter);
+    }, [filter]);
+
+    function setDisplayProducts(filter) {
+        if (filter == "all") {
+            getAllProducts();
+        } else if (filter === "name") {
+            getProductsByName(values.searchName);
+        } else {
+            getProductsByCategory(filter);
+        }
+    }
 
     const getAllProducts = async () => {
         const response = await defaultGetRequest(`${baseURL}porduct/all/`);
-        products = response;
+        if (response) {
+            setProducts(response.data);
+        }
     };
+
+    const getProductsByCategory = async () => {
+        const response = await defaultGetRequest(`${baseURL}porduct/category/${filter}`);
+        if (response) {
+            setProducts(response.data);
+        }
+    };
+
+    const getProductsByName = async () => {
+        const response = await defaultGetRequest(`${baseURL}porduct/name/${values.searchName}`);
+        if (response) {
+            setProducts(response.data);
+        } else {
+            //TODO notify no products found with that name
+        }
+    };
+
+    function searchByName() {
+        updateFilter("name");
+    }
+    function updateFilter(string) {
+        setFilter(string);
+    }
 
     return (
         <React.Fragment>
@@ -27,7 +69,7 @@ const Customer = () => {
                     <Link to="/customer/account">Account</Link>
                 </div>
                 <div className="col-2">
-                    My Cart -- Items: {shoppingCart.length} Total: ${sum}
+                    My Cart -- Items: {shoppingCart.length} Total: ${total}
                 </div>
             </div>
             <div className="row">
@@ -60,9 +102,23 @@ const Customer = () => {
                 </div>
             </div>
 
-            <FlexNav data={links}></FlexNav>
+            <FlexNav data={categories}></FlexNav>
 
-            <div className="">Search Bar goes here</div>
+            <div className="">
+                <form onSubmit={handleSubmit} className="d-flex">
+                    <input
+                        className="form-control me-sm-2"
+                        type="text"
+                        placeholder="Search by name"
+                        name="searchName"
+                        value={values.searchName || ""}
+                        onChange={handleChange}
+                    />
+                    <button className="btn btn-secondary my-2 my-sm-0" type="submit">
+                        Search
+                    </button>
+                </form>
+            </div>
             <div className="">Products Go here</div>
             <ProductDisplay products={products} />
         </React.Fragment>
