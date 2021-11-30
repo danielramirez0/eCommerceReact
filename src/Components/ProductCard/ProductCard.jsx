656d3d0ff6378682d8fede1384961fe910585864
Ben Bissett <benjaminbissett@gmail.com>
2021-11-30 09:48:37 -0700

    Added a ProductImages folder to the public folder. Edited the ProductCard to allow for thumbnails to dynamicly display when products are displayed.

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
+
 const ProductCard = (props) => {
+    let productName = props.product.name.replace('"', "");
+    let imgSrc = productName + ".jpg";
 
     return (
         <div className="col" style={{ maxWidth: "15rem"  }}>
             <div className="card-header">{props.product.name}</div>
             <div className="card-body">
-                <img src="#" alt="#" />
+                <img src={`/ProductImages/${imgSrc}`} width="100rem"/>
                 <p>{props.product.description}</p>
                 <p>${props.product.price}</p>
             </div>
