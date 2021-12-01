a6a56117c7882baa8af1c788f7ce755b0c2d1897
Daniel Ramirez <zoerunner@gmail.com>
2021-11-30 21:34:34 -0800

    Adds order history to account

--- src/Components/Account/Account.jsx
+++ src/Components/Account/Account.jsx
@@ -1,28 +1,125 @@
-import React, { useContent, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { BaseURLContext } from "../../baseURL-context";
 import { protectedEnpointGetRequest } from "../../static/functions";
-import { Card } from "react-bootstrap";
+import { Card, Table } from "react-bootstrap";
 import useForm from "../useForm/useForm";
 import useAuth from "../useAuth/useAuth";
 import { useNavigate } from "react-router";
 import FlexNav from "../FlexNav/FlexNav";
 import logo from "../../img/LogoWithName.svg";
 import jwtDecode from "jwt-decode";
+import dateFormat from "dateformat";
 
 const Account = (props) => {
     const { values, handleChange, handleSubmit } = useForm(null);
     const [accountData, setAccountData] = useState();
+    const { baseURL } = useContext(BaseURLContext);
     const navigate = useNavigate();
     const auth = useAuth();
+    const [orderHistory, setOrderHistory] = useState([]);
+    const [activeOrder, setActiveOrder] = useState();
 
     useEffect(() => {
         if (auth.jwt) {
             setAccountData(jwtDecode(auth.jwt));
+            getOrderHistory();
         } else {
             navigate("/");
         }
     }, []);
+
+    useEffect(() => {
+        renderOrderHistory();
+    }, [orderHistory]);
+
+    useEffect(() => {
+        renderOrderDetail();
+    }, [activeOrder]);
+
+    async function getOrderHistory() {
+        const response = await protectedEnpointGetRequest(`${baseURL}order/user`, auth.jwt);
+        if (response) {
+            setOrderHistory(response.data);
+        }
+    }
+
+    async function getOrderDetails(id) {
+        const response = await protectedEnpointGetRequest(`${baseURL}order/detail/${id}`, auth.jwt);
+        console.log(response);
+        if (response) {
+            return response.data;
+        } else {
+            return [];
+        }
+    }
+
+    async function renderOrderDetail() {
+        const products = await getOrderDetails(activeOrder);
+        return (
+            <table className="table" id={`order${activeOrder}`}>
+                <thead>
+                    <tr>
+                        <th>Product Id</th>
+                        <th>Name</th>
+                        <th>Price Paid</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) => (
+                        <tr>
+                            <th>
+                                {product.product.id}
+                                {product.product.name}
+                                {product.price}
+                                {product.quantity}
+                            </th>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+    }
+
+    function renderOrderHistory() {
+        return (
+            <>
+                <table className="table" id="orderHistory">
+                    <thead>
+                        <tr>
+                            <th scope="col">Order Number</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Total</th>
+                            <th scope="col">*Future Release* Details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orderHistory &&
+                            orderHistory.map((order) => (
+                                <tr>
+                                    <th scope="row" className="col-4">
+                                        {" "}
+                                        {order.id}{" "}
+                                    </th>
+                                    <td className="col-4">{dateFormat(order.date)}</td>
+                                    <td className="col-4">{order.total}</td>
+                                    <td>
+                                        <button
+                                        disabled
+                                            className="btn btn-primary disabled"
+                                            onClick={() => setActiveOrder(order.id)}
+                                        >
+                                            View
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </>
+        );
+    }
 
     return (
         <React.Fragment>
@@ -31,8 +128,7 @@ const Account = (props) => {
                 <div className="col-2">
                     <Link to="/customer/account">Account</Link>
                 </div>
-                <div className="col-2">
-                </div>
+                <div className="col-2"></div>
                 <div className="col-1">
                     <Link to="/logoff">Logoff</Link>
                 </div>
@@ -66,7 +162,12 @@ const Account = (props) => {
                     </Card>
                 </div>
             </div>
-            <FlexNav data={[]} callback={(value) => navigate(value)} callbackParam="/customer" buttonText="Continue shopping"></FlexNav>
+            <FlexNav
+                data={[]}
+                callback={(value) => navigate(value)}
+                callbackParam="/customer"
+                buttonText="Continue shopping"
+            ></FlexNav>
             <h1>Account Details</h1>
             <table className="table table-hover" id="shoppingcart">
                 <thead>
@@ -124,6 +225,12 @@ const Account = (props) => {
                     </tr>
                 </tbody>
             </table>
+            <div className="row">
+                <div className="col">
+                    <h3>Order History</h3>
+                    {renderOrderHistory()}
+                </div>
+            </div>
         </React.Fragment>
     );
 };
